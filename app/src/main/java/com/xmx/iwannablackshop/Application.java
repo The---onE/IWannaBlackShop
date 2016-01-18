1c79a116cefdac1b650f173447107c142853704d
The-_-onE <834489218@qq.com>
2016-01-18 17:43:53 +0800

    Could Push Message to every User

--- app/src/main/java/com/xmx/iwannablackshop/Application.java
+++ app/src/main/java/com/xmx/iwannablackshop/Application.java
@@ -1,9 +1,12 @@
 package com.xmx.iwannablackshop;
 
+import com.avos.avoscloud.AVInstallation;
 import com.avos.avoscloud.AVOSCloud;
+import com.avos.avoscloud.PushService;
 import com.avos.avoscloud.im.v2.AVIMMessageManager;
 import com.avos.avoscloud.im.v2.messages.AVIMTextMessage;
 import com.xmx.iwannablackshop.Chat.MessageHandler;
+import com.xmx.iwannablackshop.PushMessage.ReceiveMessageActivity;
 
 /**
  * Created by The_onE on 2016/1/3.
@@ -15,5 +18,7 @@ public class Application extends android.app.Application {
         AVOSCloud.initialize(this, "jg8rpu25f2dTGU4dSWLo96tg-gzGzoHsz", "6NdDmnjpXWSID9LCFzBO3CPj");
         AVIMMessageManager.registerMessageHandler(AVIMTextMessage.class, new MessageHandler(this));
         //AVIMMessageManager.registerMessageHandler(AVIMMessage.class, new MessageBaseHandler(this));
+        AVInstallation.getCurrentInstallation().saveInBackground();
+        PushService.setDefaultPushCallback(this, ReceiveMessageActivity.class);
     }
 }
---
+++ app/src/main/java/com/xmx/iwannablackshop/PushMessage/ReceiveMessageActivity.java
@@ -0,0 +1,23 @@
+package com.xmx.iwannablackshop.PushMessage;
+
+import android.os.Bundle;
+
+import com.xmx.iwannablackshop.ActivityBase.BaseTempActivity;
+import com.xmx.iwannablackshop.R;
+
+public class ReceiveMessageActivity extends BaseTempActivity {
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_receive_message);
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+
+    @Override
+    protected void processLogic(Bundle savedInstanceState) {
+
+    }
+}
