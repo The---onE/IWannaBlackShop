5ecbdbd77c995ed973c2df8c298b6041748fec50
The-_-onE <834489218@qq.com>
2016-01-19 23:08:29 +0800

    Could receive Url of File from Pushed

--- app/src/main/java/com/xmx/iwannablackshop/PushMessage/ReceiveMessageActivity.java
+++ app/src/main/java/com/xmx/iwannablackshop/PushMessage/ReceiveMessageActivity.java
@@ -1,9 +1,14 @@
 package com.xmx.iwannablackshop.PushMessage;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.xmx.iwannablackshop.ActivityBase.BaseTempActivity;
 import com.xmx.iwannablackshop.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ReceiveMessageActivity extends BaseTempActivity {
     @Override
@@ -18,6 +23,27 @@ public class ReceiveMessageActivity extends BaseTempActivity {
 
     @Override
     protected void processLogic(Bundle savedInstanceState) {
+        try {
+            String data = getIntent().getStringExtra("com.avos.avoscloud.Data");
+            JSONObject json = new JSONObject(data);
 
+            String alert = json.getString("alert");
+            if (alert != null) {
+                setTitle(alert);
+            }
+
+            try {
+                EditText urlView = getViewById(R.id.file_url);
+                String url = json.getString("file_url");
+                if (url != null) {
+                    urlView.setText(url);
+                }
+            } catch (JSONException e) {
+                EditText urlView = getViewById(R.id.file_url);
+                urlView.setVisibility(View.INVISIBLE);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
